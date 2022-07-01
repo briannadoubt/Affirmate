a9603c341f4577f8e23f9178958cac5ac004c755
Brianna Doubt <bri@briannadoubt.com>
2022-07-01 15:22:36 -0700

    Initial Commit

---
+++ Affirmate/AffirmateApp.swift
@@ -0,0 +1,17 @@
+//
+//  AffirmateApp.swift
+//  Affirmate
+//
+//  Created by Bri on 7/1/22.
+//
+
+import SwiftUI
+
+@main
+struct AffirmateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AffirmateUITests/AffirmateUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  AffirmateUITestsLaunchTests.swift
+//  AffirmateUITests
+//
+//  Created by Bri on 7/1/22.
+//
+
+import XCTest
+
+final class AffirmateUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
