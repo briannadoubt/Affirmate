d9c95d0dbf85e18d1e7c680e64bdb692fbeee7c8
Brianna Zamora <brianna.zamora@rewardstyle.com>
2023-01-12 16:28:34 -0800

    Move testing extensions to separate file

--- AffirmateServer/Tests/AffirmateServerTests/Application+Testable.swift
+++ AffirmateServer/Tests/AffirmateServerTests/Application+Testable.swift
@@ -5,20 +5,80 @@
 //  Created by Bri on 8/6/22.
 //
 
-@testable import XCTVapor
+import XCTVapor
 @testable import AffirmateServer
+import AffirmateShared
 
 extension Application {
-//    static func testable() throws -> Application {
-//        let app = Application(.testing)
-//        defer { app.tearDown() }
-//        try configure(app)
-//        try app.autoRevert().wait()
-//        try app.autoMigrate().wait()
-//        return app
-//    }
-    
-//    func tearDown() {
-//        self.shutdown()
-//    }
+    func setUp() throws {
+        try configure(self)
+        do { try autoRevert().wait() } catch { print("Failed to auto revert", error) }
+        do { try autoMigrate().wait() } catch { print("Failed to auto migrate:", error) }
+    }
+
+    func tearDown() {
+        do { try autoRevert().wait() } catch { print("Failed to auto revert", error) }
+    }
+
+    @discardableResult
+    func signUp(firstName: String = "Meow", lastName: String = "Face", username: String = "meowface", email: String = "meow@fake.com", password: String = "Test123$", confirmPassword: String = "Test123$") async throws -> Application {
+        try await test(.POST, "/auth/new/") { request in
+            let userCreate = UserCreate(firstName: firstName, lastName: lastName, username: username, email: email, password: password, confirmPassword: password)
+            try request.content.encode(userCreate, using: JSONEncoder())
+        } afterResponse: { response in
+            XCTAssertEqual(response.status, .ok)
+
+            let optionalUser = try await User.query(on: db).all().first
+            let user = try XCTUnwrap(optionalUser)
+            XCTAssertEqual(user.firstName, firstName)
+            XCTAssertEqual(user.lastName, lastName)
+            XCTAssertEqual(user.username, username)
+            XCTAssertEqual(user.email, email)
+            XCTAssertTrue(try Bcrypt.verify(password, created: user.passwordHash))
+        }
+        return self
+    }
+
+    @discardableResult
+    func login(firstName: String = "Meow", lastName: String = "Face", username: String = "meowface", email: String = "meow@fake.com", password: String = "Test123$") async throws -> Application {
+        try await test(.GET, "/auth/login/") { request in
+            request.headers.basicAuthorization = BasicAuthorization(username: username, password: password)
+        } afterResponse: { response in
+            XCTAssertEqual(response.status, .ok)
+
+            let optionalSessionToken = try await SessionToken.query(on: db).all().first
+            let sessionToken = try XCTUnwrap(optionalSessionToken)
+
+            let loginResponse = try response.content.decode(UserLoginResponse.self)
+
+            let optionalUser = try await User.query(on: db).with(\.$chatInvitations).all().first
+            let user = try XCTUnwrap(optionalUser)
+
+            XCTAssertEqual(loginResponse.sessionToken.value, sessionToken.value)
+            XCTAssertEqual(loginResponse.user.firstName, user.firstName)
+            XCTAssertEqual(loginResponse.user.firstName, firstName)
+            XCTAssertEqual(loginResponse.user.lastName, user.lastName)
+            XCTAssertEqual(loginResponse.user.lastName, lastName)
+            XCTAssertEqual(loginResponse.user.username, user.username)
+            XCTAssertEqual(loginResponse.user.username, username)
+            XCTAssertEqual(loginResponse.user.email, user.email)
+            XCTAssertEqual(loginResponse.user.email, email)
+            XCTAssertEqual(loginResponse.user.chatInvitations.isEmpty, user.chatInvitations.isEmpty)
+        }
+        return self
+    }
+
+    @discardableResult
+    func logout() async throws -> Application {
+        try await test(.POST, "/auth/logout/") { request in
+            let optionalSessionToken = try await SessionToken.query(on: db).all().first
+            let sessionToken = try XCTUnwrap(optionalSessionToken)
+            request.headers.bearerAuthorization = BearerAuthorization(token: sessionToken.value)
+        } afterResponse: { response in
+            XCTAssertEqual(response.status, .ok)
+            let optionalSessionToken = try await SessionToken.query(on: db).all().first
+            XCTAssertNil(optionalSessionToken)
+        }
+        return self
+    }
 }
